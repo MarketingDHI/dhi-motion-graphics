e78dc8efb9e7884539f794777db50f43e9ab33af
Christine Brovkina <christine.brovkina@digitalhubindustry.de>
2024-10-21 09:44:05 +0200

    demo mit json from jitter

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import lottie from 'lottie-web'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const animationContainer = document.querySelector('.lottie-container')
 
-setupCounter(document.querySelector('#counter'))
+lottie.loadAnimation({
+  container: animationContainer, // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/animations/dhi-alt.json' // the path to the animation json
+});
